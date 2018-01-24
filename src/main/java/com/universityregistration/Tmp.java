514bb4dbbf2134d1afb610a6e27755a658d3183c
Argos <Argos@4k-tv>
2018-01-24 11:59:04 -0500

    second commit

--- src/main/java/com/universityregistration/Tmp.java
+++ src/main/java/com/universityregistration/Tmp.java
@@ -1,7 +1,6 @@
 package com.universityregistration;
-
 public class Tmp {
 	public static void main(String[] args){
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
 	}
 }
