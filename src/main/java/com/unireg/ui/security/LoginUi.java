05946bd458db278d93aa1b57c89a543add6e159e
Argos <Argos@4k-tv>
2018-02-25 13:17:12 -0500

    added titles to landing pages

--- src/main/java/com/unireg/ui/security/LoginUi.java
+++ src/main/java/com/unireg/ui/security/LoginUi.java
@@ -1,16 +1,19 @@
 package com.unireg.ui.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
 
 @SpringUI(path = LoginUi.PATH)
+@Title(LoginUi.TITLE)
 @Theme(LoginUi.THEME)
 public class LoginUi extends UI {
 	private static final long serialVersionUID = 1L;
 	public static final String PATH = "/login";
 	public static final String THEME = "valo";
+	public static final String TITLE = "Zay's University App";
 	@Autowired
 	private LoginFactory loginFactory;
 	
--- src/main/java/com/unireg/ui/security/SignupUi.java
+++ src/main/java/com/unireg/ui/security/SignupUi.java
@@ -1,16 +1,19 @@
 package com.unireg.ui.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
 
 @SpringUI(path = SignupUi.PATH)
+@Title(SignupUi.TITLE)
 @Theme(SignupUi.THEME)
 public class SignupUi extends UI {
 	private static final long serialVersionUID = 1176981398310394304L;
 	public static final String PATH = "/signup";
 	public static final String THEME = "valo";
+	public static final String TITLE = "Zay's University App";
 	@Autowired
 	private SignupFactory signupFactory;
 	
