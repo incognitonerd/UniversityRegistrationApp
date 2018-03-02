07e3287184c5a3455e4021eae01de738bd1770de
Argos <Argos@172.30.61.253>
2018-03-01 19:45:27 -0500

    changed banner color

--- src/main/java/com/unireg/utils/Constants.java
+++ src/main/java/com/unireg/utils/Constants.java
@@ -19,7 +19,7 @@ public enum Constants{
 	PASSWORD("Password"), //
 	SIGN_UP("Sign Up"), //
 	//
-	LOGO_BANNER("../../images/resizedHeaderBanner.jpeg"),
+	LOGO_BANNER("../../images/yellowResized.png"),
 	//
 	STUDENTS_PARENT("STUDENTS"), //
 	STUDENTS_ADD_STUDENT("Add Student"), //
